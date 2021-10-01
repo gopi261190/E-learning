f894b6b71047e3ba9a4d4765f0807f7ccd124879
GOPI CHANDRA SHARMA <63851794+gopi261190@users.noreply.github.com>
2021-10-01 09:49:27 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// $(".toggle").click(function () {
+//     $('ul').slideToggle('medium', function () {
+//         if ($(this).is(':visible'))
+//             $(this).css('display', 'flex');
+//     });
+// });
+
+$(".toggle").click(function (){
+    $("ul").slideToggle();
+});
